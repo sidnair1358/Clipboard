3973c9f5c815378031153f8d19e3a448f27aa50d
sidn <siddharth.nair@bravissimo.com>
2024-06-26 12:51:20 +0100

    clipboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    // In order to define screen sizes
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      // In order to add custom colour
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+      fontFamily: {
+        sans: ["Bai Jamjuree", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
